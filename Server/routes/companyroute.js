0fbd214bbd29899d6496d71e2c871c8488f1c7a2
SHREYAS TIWARI <112263176+Shreyastiwari30@users.noreply.github.com>
2025-08-28 21:12:08 +0530

    Update companyroute.js

--- Server/routes/companyroute.js
+++ Server/routes/companyroute.js
@@ -1,13 +1,12 @@
-import express from 'express';
-import isauthenticated from '../middlewares/authenticated.js'
-import {login,register,updateProfile,logout} from '../controllers/user.controller.js';
-import { getcompany, getcompanyById, registerCompany, updateCompany } from '../controllers/company.controller.js';
+import express from "express";
+import isAuthenticated from "../middlewares/authenticated.js";
+import { getcompany, getcompanyById, registerCompany, updateCompany } from "../controllers/company.controller.js";
 
-const router= express.Router();
+const router = express.Router();
 
-router.route('/register').post(isauthenticated,registerCompany);
-router.route('/get').get(isauthenticated,getcompany);
-router.route('/update/:id').put(isauthenticated,updateCompany);
-router.route('/get/:id').get(isauthenticated,getcompanyById)
+router.route("/register").post(isAuthenticated, registerCompany);
+router.route("/get").get(isAuthenticated, getcompany);
+router.route("/update/:id").put(isAuthenticated, updateCompany);
+router.route("/get/:id").get(isAuthenticated, getcompanyById);
 
 export default router;
